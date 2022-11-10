4562e7bd9a3078e84aad46ab192e30e4502f3760
juanhaugaard <juanhaugaard@gmail.com>
2022-11-10 12:17:19 -0500

    Moved JOOQ generated code to its own jar module
    New module jooq-generated-sakila to contain JOOQ generated sakila code, changed package root to org from com, removed package generated.

---
+++ sakila-data-generator/src/main/java/org/tayrona/sakila/data/utils/JooqExceptionTranslator.java
@@ -0,0 +1,17 @@
+package org.tayrona.sakila.data.utils;
+
+import org.jooq.ExecuteContext;
+import org.jooq.ExecuteListener;
+import org.jooq.SQLDialect;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
+
+import java.util.Objects;
+
+public class JooqExceptionTranslator implements ExecuteListener {
+    public void exception(ExecuteContext context) {
+        SQLDialect dialect = context.configuration().dialect();
+        SQLExceptionTranslator translator = new SQLErrorCodeSQLExceptionTranslator(dialect.name());
+        context.exception(translator.translate("Access database using Jooq", context.sql(), Objects.requireNonNull(context.sqlException())));
+    }
+}
